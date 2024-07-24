e50da9ac464abe5e16a12ce4743f9df4aeeb34d1
Amogh.Kharche <Amogh.Kharche@DELL1536>
2024-07-24 09:59:26 +0530

    Add project files.

---
+++ StudentEncapApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEncapApp2
+{
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(1, "John", 8.5);
+            Student student2 = new Student(2, "Jane", 11);
+            Student student3 = new Student(3,"Ramesh", -10);
+
+            
+            Console.WriteLine("Student 1 details:");
+            student1.PrintDetails();
+
+
+            Console.WriteLine("Student 2 details:");
+            student2.PrintDetails();
+
+
+            Console.WriteLine("Student 3 details:");
+            student3.PrintDetails();
+        }
+    }
+}
---
+++ StudentEncapApp2/Student.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace StudentEncapApp2
+{
+    internal class Student
+    {
+        private uint _studentRollNo;
+        private string _studentName;
+        private double _cgpa;
+
+        public uint StudentRollNo
+        {
+            get { return _studentRollNo; }
+            set
+            {
+                if (value > 0)
+                    _studentRollNo = value;
+                else
+                    _studentRollNo = 0;
+            }
+        }
+
+        public string StudentName
+        {
+            get { return _studentName; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value) && value.Length >= 5)
+                    _studentName = value;
+                else
+                    _studentName = "";
+            }
+        }
+
+        public double CGPA
+        {
+            get { return _cgpa; }
+            set
+            {
+                if (value < 1)
+                    _cgpa = 1;
+                else if (value > 10)
+                    _cgpa = 10;
+                else
+                    _cgpa = value;
+            }
+        }
+
+        public Student(uint rollNo, string name, double cgpa)
+        {
+            StudentRollNo = rollNo;   
+            StudentName = name;       
+            CGPA = cgpa;              
+        }
+
+        public double CalculatePercentage()
+        {
+            return CGPA * 10;
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Roll No: {StudentRollNo}");
+            Console.WriteLine($"Name: {StudentName}");
+            Console.WriteLine($"CGPA: {CGPA}");
+            Console.WriteLine($"Percentage: {CalculatePercentage()}%");
+            Console.WriteLine();
+        }
+    }
+}
